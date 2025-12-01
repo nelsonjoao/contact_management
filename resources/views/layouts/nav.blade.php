1e3de922057de1d92cccaae85fecfe96ae1a4476
nelsonjoao <76785554+nelsonjoao@users.noreply.github.com>
2025-12-01 12:35:10 +0100

    Add layout base

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,16 @@
+ 	<div class="">
+           <div class="row py-2 bg-dark text-white">
+		<div class="col-sm-12">
+				<h3>Contact Management</h3>
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="col-sm-12 pt-2">
+				<a href="" class="btn btn-dark"> Home </a>
+				<a href="" class="btn btn-dark"> New Contact </a>
+		</div>
+	</div>
+    </div>
+ 
+	<hr>
