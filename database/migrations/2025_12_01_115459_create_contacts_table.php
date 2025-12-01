cf08b0dac23e7c80e6f2320caadbb2dfd710b5b1
nelsonjoao <76785554+nelsonjoao@users.noreply.github.com>
2025-12-01 13:40:10 +0100

    update migration users

--- database/migrations/2025_12_01_115459_create_contacts_table.php
+++ database/migrations/2025_12_01_115459_create_contacts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email');
-            $table->string('contact');
+            $table->string('contact',9);
             $table->timestamps();
             $table->softDeletes();
         });
